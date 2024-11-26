c516d8059d98aea7822afddb0808e6b292189028
GenericP3rson <genericp3rson@gmail.com>
2024-11-26 08:41:42 -0500

    [minor] cpu code bugfix

--- cpu/glwe/glwe.py
+++ cpu/glwe/glwe.py
@@ -8,7 +8,7 @@ N = 100
 q = 2**16
 p = 2**10
 # security param for how much noise, keep small if large constant multiplication
-LAMBDA = 3
+LAMBDA = 1
 
 # s = [
 #     [0, 1, 1, 0], # x^2 + x 
@@ -63,24 +63,24 @@ def make_num(list, bits):
         number += (val << i*bits)
     return number
 
-with open(f'/Users/ruth/6.2050/fpga-project/lab03/data/A.mem', 'w') as f:
+with open(f'A.mem', 'w') as f:
     for x in range(k):
         for y in range(0, N, 2):
             f.write(f'{make_num(A[x][y:y+2], 16):X}\n')
 print('Output image saved at A.mem')
 
-with open(f'/Users/ruth/6.2050/fpga-project/lab03/data/s.mem', 'w') as f:
+with open(f's.mem', 'w') as f:
     for x in range(k):
         for y in range(0, N, 2):
             f.write(f'{make_num(s[x][y:y+2], 1):X}\n')
 print('Output image saved at s.mem')
 
-with open(f'/Users/ruth/6.2050/fpga-project/lab03/data/pt.mem', 'w') as f:
+with open(f'pt.mem', 'w') as f:
     for y in range(0, N, 2):
         f.write(f'{make_num(m[y:y+2], 1):X}\n')
 print('Output image saved at pt.mem')
 
-with open(f'/Users/ruth/6.2050/fpga-project/lab03/data/b.mem', 'w') as f:
+with open(f'b.mem', 'w') as f:
     for x in range(k):
         for y in range(0, N, 2):
             f.write(f'{0}\n')
@@ -90,10 +90,11 @@ def enc():
     # E = [1, 0, 1, -1] # \in q
     delta = q / p
     # print(delta, LAMBDA, delta/LAMBDA)
-    E = np.random.randint(-delta/LAMBDA, delta/LAMBDA, N)
+    # E = np.random.randint(-delta/LAMBDA, delta/LAMBDA, N)
     delta_m = np.array(m) * delta
 
-    B = np.array(delta_m) + np.array(E)
+    # B = np.array(delta_m) + np.array(E)
+    B = np.array(delta_m)
 
     # breakpoint()
     for idx in range(len(A)):
@@ -104,12 +105,12 @@ def enc():
     # print(B)
     return B
 
-def dec(B):
-    B_res = np.array(B)
+def dec(B, c = 1):
+    B_res = (np.array(B) * c) % q
     for idx in range(len(A)):
-        B_res = B_res - np.array(polynomial_mult(A[idx], s[idx], N, q))
+        B_res = (B_res - np.array(polynomial_mult((c**2 * A[idx]) % q, (s[idx]) % q, N, q))) % q
     # can check bottom bits and add one if needed
-    return np.round(B_res / (q / p))  % p
+    return np.round(B_res / (q / p)) / c
 
 """
 Operations on ciphertext
@@ -122,7 +123,10 @@ def add_constant(ct, c):
     return ct + c * q/p
 
 def mul_constant(ct, c):
-    return (c * ct)
+    # print("CT", dec(ct))
+    # print("2 * CT", dec(2*ct))
+    # print("2 * CT", dec(ct + ct))
+    return ((c * ct) % (q))
 
 
 
@@ -142,10 +146,11 @@ print("Average time taken: ", sum(timing)/100)'''
 
 print("inp:", np.array(m) % p)
 print("eq: 2x + 1")
-print("pt computation:", (2 * (np.array(m) % p)) % p)
-print("ct computation:", dec(mul_constant(enc(), 2)))
+print("pt computation:", (3 * (np.array(m) % p)) % q)
+print("ct computation:", dec(mul_constant(enc(), 3), c=3))
+# assert np.all(((1 * (np.array(m) % q/p)) % q/p) == dec(mul_constant(enc(), 2)))
 
-print("pt computation:", ((np.array(m) % p) + 1) % p)
-print("ct computation:", dec(add_constant(enc(), 1)))
+# print("pt computation:", ((np.array(m) % p) + 1) % p)
+# print("ct computation:", dec(add_constant(enc(), 1)))
 
 print(dec(enc()))
